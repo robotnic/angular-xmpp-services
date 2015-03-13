46e3c4a7c67809b7187bd7941f3d7b66fcfa664f
bernhard <bz@datenkueche.com>
2015-03-13 15:57:11 +0100

    roster.add jid buddycloud -> api -> that

--- src/services/buddycloud.js
+++ src/services/buddycloud.js
@@ -476,7 +476,7 @@ angular.module('BuddycloudModule', [])
                             //add user to roster !!!!!!!!!!!!!!!!!!!
 
                             console.log(that.data.currentnode);
-                            var jid=api.xmpp.parseNodeString(buddycloud.data.currentnode);
+                            var jid=that.xmpp.parseNodeString(that.data.currentnode);
                             api.xmpp.send('xmpp.roster.add', {
                                 "jid": jid
                             }).then(function(data){
